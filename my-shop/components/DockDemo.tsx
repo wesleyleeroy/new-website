e2e0b6354adce78d140cb660dde4ed92422e2259
wesleyleeroy <wesleyleeroy06@gmail.com>
2026-02-12 15:38:00 -0500

    w

--- my-shop/components/DockDemo.tsx
+++ my-shop/components/DockDemo.tsx
@@ -77,7 +77,7 @@ const Icons = {
 const DATA = {
     navbar: [
         { href: "/", icon: HomeIcon, label: "Home" },
-        { href: "https://www.researchgate.net/scientific-contributions/Wesley-Leeroy-2336487418", icon: Icons.researchgate, label: "ResearchGate" },
+        { href: "https://www.researchgate.net/scientific-contributions/Wesley-Leeroy-2338827419", icon: Icons.researchgate, label: "ResearchGate" },
     ],
     contact: {
         social: {
